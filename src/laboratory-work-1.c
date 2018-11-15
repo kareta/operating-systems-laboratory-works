5bde9a8f97eeaadb8f6fc8d2d1ecee5b93a2fbb4
Victor Kareta <karetavictor@gmail.com>
2018-11-15 22:51:59 +0200

    Print file names in current directory

--- src/laboratory-work-1.c
+++ src/laboratory-work-1.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <sys/types.h>
 #include <dirent.h>
+#include <stdlib.h>
 #include "laboratory-work-1.h"
 
 #define CURRENT_DIRECTORY "./"
@@ -8,9 +9,34 @@
 int main() 
 { 
     const int file_count = get_file_count(CURRENT_DIRECTORY);
-    printf("%i", file_count);
+    struct dirent* directory_entries = get_directory_content(CURRENT_DIRECTORY);
+    for (int i = 0; i < file_count; i++) {
+        puts(directory_entries[i].d_name);   
+    }
 
     return 0;
+}
+
+struct dirent* get_directory_content(const char* directory_path)
+{
+    DIR *directory = opendir(directory_path);
+    if (directory == NULL) {
+        perror("Couldn't open the directory");
+        return NULL;
+    }
+
+    const int dirent_size = sizeof(struct dirent);
+    const int file_count = get_file_count(directory_path);
+    const int directory_entries_size = dirent_size * file_count;
+
+    struct dirent* directory_entries = malloc(directory_entries_size);
+    for (int i = 0; i < file_count; i++) {
+        directory_entries[i] = *readdir(directory);
+    }
+    
+    closedir(directory);
+
+    return directory_entries;
 }
 
 int get_file_count(const char* directory_path) 
@@ -25,6 +51,8 @@ int get_file_count(const char* directory_path)
     while (readdir(directory)) {
         file_count++;   
     }
+
+    closedir(directory);
 
     return file_count;
 }
--- src/laboratory-work-1.h
+++ src/laboratory-work-1.h
@@ -3,6 +3,8 @@
 
 #include <dirent.h>
 
+struct dirent* get_directory_content(const char* directory_path);
+
 int get_file_count(const char* directory_path);
 
 #endif
