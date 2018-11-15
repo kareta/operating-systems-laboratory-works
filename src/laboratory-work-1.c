98e27922e973780c41c7b643454ec5b8b96e34fb
Victor Kareta <karetavictor@gmail.com>
2018-11-15 23:58:13 +0200

    Sort files by creation time

--- src/laboratory-work-1.c
+++ src/laboratory-work-1.c
@@ -11,15 +11,17 @@ int main()
 { 
     const int file_count = get_file_count(CURRENT_DIRECTORY);
     struct dirent* directory_entries = get_directory_content(CURRENT_DIRECTORY);
+    int dirent_size = sizeof(struct dirent);
+    qsort(directory_entries, file_count, dirent_size, &compare_files);
     struct stat file_stat;
     for (int i = 0; i < file_count; i++) {
         const char* file_name = directory_entries[i].d_name;
-        int file_creation_time = get_file_creation_time(file_name);
         if (is_directory(file_name)) {
             printf("d ");
         } else {
             printf("f ");
         }
+        int file_creation_time = get_file_creation_time(file_name);
         printf("%d %s\n", file_creation_time, file_name);   
     }
 
@@ -82,5 +84,19 @@ int is_directory(const char *file_name)
 
 int compare_files(const void* first_file, const void* second_file)
 {
-    return 1;
+    const char* first_file_name = ((struct dirent*) first_file)->d_name;
+    const char* second_file_name = ((struct dirent*) second_file)->d_name;
+
+    int first_file_creation_time = get_file_creation_time(first_file_name);
+    int second_file_creation_time = get_file_creation_time(second_file_name);
+    
+    if (is_directory(first_file_name) && !is_directory(second_file_name)) {
+        return 1;
+    }
+
+    if (!is_directory(first_file_name) && is_directory(second_file_name)) {
+        return -1;
+    }
+
+    return second_file_creation_time - first_file_creation_time;
 }
